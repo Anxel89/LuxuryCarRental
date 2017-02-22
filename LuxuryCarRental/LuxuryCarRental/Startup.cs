8634cf6223cd1eec0401fe1be52b1a5406403a9f
Mattew Robison <anxel9302@gmail.com>
2017-02-21 22:43:06 -0500

    creation of project

---
+++ LuxuryCarRental/LuxuryCarRental/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LuxuryCarRental.Startup))]
+namespace LuxuryCarRental
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
